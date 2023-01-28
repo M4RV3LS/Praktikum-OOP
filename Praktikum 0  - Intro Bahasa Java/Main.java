3df8bef15edff2a41e66890d8dad428c040a7dae
MarvelSubekti <16521220@mahasiswa.itb.ac.id>
2023-01-28 15:16:06 +0700

    Praktikum 0

---
+++ Praktikum 0  - Intro Bahasa Java/Main.java
@@ -0,0 +1,30 @@
+/*Praktikum 0*/
+//Topik : Intro Bahasa Java
+// Nama : Marvel Subekti 
+// NIM : 182121058
+// Kelas : K02
+
+import java.lang.System;
+import java.util.Scanner;
+
+public class Main 
+{  
+    /* Driver Code */  
+
+    /* Buatlah sebuah program bernama Main.java yang jika dijalankan melakukan print "Hello World".*/
+    //No 1
+    // public static void main(String ar[])   
+    // {
+    //     String output = "Hello World";
+    //     System.out.println(output);  
+    // }  
+
+    /*Buatlah program Java bernama Main.java yang menerima input integer dan 
+    memberikan output 2 kali nilai integer tersebut. Output diakhiri dengan newline.*/
+    //No 2
+    // public static void main(String[] args) {
+    //     Scanner input = new Scanner(System.in);
+    //     int x = input.nextInt();
+    //     System.out.println(2 * x);
+    // }
+}  
